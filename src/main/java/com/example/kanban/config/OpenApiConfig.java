490f031dae3efaabbb5dff25095b449be18d084d
npq-thien <thiennguyen3132002@gmail.com>
2024-09-26 16:05:49 +0700

    authentication with token for swagger

--- src/main/java/com/example/kanban/config/OpenApiConfig.java
+++ src/main/java/com/example/kanban/config/OpenApiConfig.java
@@ -1,7 +1,11 @@
 package com.example.kanban.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -10,7 +14,15 @@ import org.springframework.context.annotation.Configuration;
                 title = "Kanban API",
                 version = "0.1",
                 description = "API documentation for the Kanban application"
-        )
+        ),
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
 )
 public class OpenApiConfig {
 }
